369999495ee693a0c47924cd3d65585143262387
Ireguii <andriu9700@gmail.com>
2025-03-15 01:32:39 -0500

    9 interfaces

---
+++ app/src/main/java/com/example/appnimbus/NotaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appnimbus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NotaActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.escribir_nota)
+  }
+}
